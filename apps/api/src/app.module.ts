a4495d721efb4fe57b24bf0d42d28d04f310b88d
davit1991-sa <d_kobrava@yahoo.com>
2026-01-31 23:59:28 +0400

    Delete apps/api directory

--- apps/api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaModule } from './prisma/prisma.module';
-
-@Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-})
-export class AppModule {}
